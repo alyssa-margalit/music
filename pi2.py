168d1501a64e021219840c69a6ec90ffd7767b67
Alyssa Margalit <alyssam0@usc.edu>
2020-11-13 15:36:26 -0800

    mixer test

--- pi2.py
+++ pi2.py
@@ -28,67 +28,57 @@ pygame.mixer.music.set_volume(15)
 #pygame.mixer.music.play()
 #while pygame.mixer.music.get_busy() == True:
 	#pass
+global queue 
+queue = "S1.mp3"
+
+def newBeat(distance):
+
+	if 0<distance<3:
+		queue = "S1.mp3"
+		print("1")
+
+	elif 3<distance<6:
+		queue = "S2.mp3"
+		print("2")
+
+	elif 6<distance<9:
+		queue = "S3.mp3"
+		print("3")
+	elif 9<distance<12:
+		queue = "S4.mp3"
+		print("4")
+	elif 12<distance<15:
+		queue = "S5.mp3"
+		print("5")
+	elif 12<distance<15:
+		queue = "S6.mp3"
+		print("6")
+	elif 15<distance<18:
+		queue = "S7.mp3"
+		print("7")
+	elif 18<distance<21:
+		queue = "S8.mp3"
+		print("8")
+	else:
+		print("101")
+
+
 while True:
 	
 	distance1 = ultrasonicRead(ranger)
 	time.sleep(.1)
 	distance2 = ultrasonicRead(ranger)
 	distance = (distance1+distance2)/2
-	#print(distance)
 	slide = analogRead(pot)
-	#print(slide)
-	if 0<distance<3:
-		queue = "S1.mp3"
-		
-		print("1")
-		#time.sleep(delay)
 
-	elif 3<distance<6:
-		queue = "S2.mp3"
-		
-		print("2")
-		#time.sleep(delay)
-	elif 6<distance<9:
-		queue = "S3.mp3"
-		
-		print("3")
-		#time.sleep(delay)
-	elif 9<distance<12:
-		queue = "S4.mp3"
-		
-		print("4")
-		#time.sleep(delay)
-	elif 12<distance<15:
-		queue = "S5.mp3"
-		
-		print("5")
-		#time.sleep(delay)
-	elif 12<distance<15:
-		queue = "S6.mp3"
-
-		print("6")
-		#time.sleep(delay)
-	elif 15<distance<18:
-		queue = "S7.mp3"
-		
-		print("7")
-		#time.sleep(delay)
-	elif 18<distance<21:
-		queue = "S8.mp3"
-		#pygame.mixer.music.queue("S8.mp3")
-		#pygame.mixer.music.play()
-		print("8")
-		#time.sleep(delay)
-	#else:
-		#time.sleep(.4)
-	#else:
 	pygame.mixer.music.play()
 	while True:
 		if pygame.mixer.music.get_busy():
+			print("busy")
 			pass
 		else:
+			newBeat(distance)
 			pygame.mixer.music.load(queue)
-			#pygame.mixer.music.play()
 			break
 		#pygame.mixer.music.play()
 	#while pygame.mixer.music.get_busy() == True:
