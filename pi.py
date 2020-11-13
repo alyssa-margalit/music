18d6ad259f3887e3dc1fff1ad9d6c711c4d2a690
Alyssa Margalit <alyssam0@usc.edu>
2020-11-13 10:44:20 -0800

    initial commit

---
+++ pi.py
@@ -0,0 +1,22 @@
+import time
+import sys
+import grovepi
+from grovepi import *
+import math
+
+ranger = 3
+pot = 2
+delay = 1
+distance = ultrasonicRead(ranger)
+print(distance)
+slide = analogRead(pot)
+print(slide)
+while True:
+	distance = ultrasonicRead(ranger)
+	print(distance)
+	slide = analogRead(pot)
+	print(slide)
+	if distance == 10:
+		delay = 2
+	time.sleep(delay)
+	
