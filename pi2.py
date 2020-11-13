dd1655796318587a9436ef2f1a5f47b27b09acaa
Alyssa Margalit <alyssam0@usc.edu>
2020-11-13 15:09:31 -0800

    mixer test

--- pi2.py
+++ pi2.py
@@ -38,40 +38,44 @@ while True:
 	#print(slide)
 	if 0<distance<3:
 		pygame.mixer.music.queue("S1.mp3")
-		
+		pygame.mixer.music.play()
 		#pygame.mixer.music.unload("S1.mp3")
 		print("1")
 		#time.sleep(delay)
 
 	elif 3<distance<6:
 		pygame.mixer.music.queue("S2.mp3")
-		
+		pygame.mixer.music.play()
 		print("2")
 		#time.sleep(delay)
 	elif 6<distance<9:
 		pygame.mixer.music.queue("S3.mp3")
-		
+		pygame.mixer.music.play()
 		print("3")
 		#time.sleep(delay)
 	elif 9<distance<12:
 		pygame.mixer.music.queue("S4.mp3")
-		
+		pygame.mixer.music.play()
 		print("4")
 		#time.sleep(delay)
 	elif 12<distance<15:
 		pygame.mixer.music.queue("S5.mp3")
+		pygame.mixer.music.play()
 		print("5")
 		#time.sleep(delay)
 	elif 12<distance<15:
 		pygame.mixer.music.queue("S6.mp3")
+		pygame.mixer.music.play()
 		print("6")
 		#time.sleep(delay)
 	elif 15<distance<18:
 		pygame.mixer.music.queue("S7.mp3")
+		pygame.mixer.music.play()
 		print("7")
 		#time.sleep(delay)
 	elif 18<distance<21:
 		pygame.mixer.music.queue("S8.mp3")
+		pygame.mixer.music.play()
 		print("8")
 		#time.sleep(delay)
 	#else:
