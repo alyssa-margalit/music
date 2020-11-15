e0beac9ca1f67a28790ebd9d2fd9e8f045848810
Alyssa Margalit <alyssam0@usc.edu>
2020-11-14 19:56:57 -0800

    timer

--- timertest.py
+++ timertest.py
@@ -3,13 +3,16 @@ from time import sleep
 
 def hello():
     print ("hello, world")
-    t = Timer(3,hello)
+    t = Timer(4,hello)
     t.start()
 
-t = Timer(3, hello)
+t = Timer(4, hello)
 t.start() # after 3 seconds, "hello, world" will be printed
 
 # timer will wake up ever 3 seconds, while we do something else
 while True:
     print ("do something else")
-    sleep(10)
+    distance1 = ultrasonicRead(ranger)
+    print(distance1)
+
+    sleep(2)
