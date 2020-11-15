e8e22eebef15e573530c2469698c4d6c58836a1a
Alyssa Margalit <alyssam0@usc.edu>
2020-11-14 19:53:18 -0800

    timer

--- timertest.py
+++ timertest.py
@@ -2,7 +2,7 @@ from threading import Timer
 from time import sleep
 
 def hello():
-    print "hello, world"
+    print ("hello, world")
     t = Timer(3,hello)
     t.start()
 
@@ -11,5 +11,5 @@ t.start() # after 3 seconds, "hello, world" will be printed
 
 # timer will wake up ever 3 seconds, while we do something else
 while True:
-    print "do something else"
+    print ("do something else")
     sleep(10)
