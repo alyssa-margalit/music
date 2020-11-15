8349e3069eae815017f4720f41ccfca4bc99f4dd
Alyssa Margalit <alyssam0@usc.edu>
2020-11-14 21:14:05 -0800

    timer

--- pi3.py
+++ pi3.py
@@ -8,7 +8,17 @@ import pygame
 from pygame.locals import *
 import os
 
+global queue
+queue = "S1.mp3"
+os.environ["SDL_VIDEODRIVER"] = "dummy"
+pygame.init()
+pygame.mixer.init()
+pygame.mixer.music.load("S1.mp3")
+queue = "S1.mp3"
+pygame.mixer.music.set_volume(15)
+
 ranger = 4
+
 #GPIO.setmode(GPIO.BCM)
 #PIR_PIN = 18
 #GPIO.setup(PIR_PIN, GPIO.IN)
@@ -17,8 +27,9 @@ ranger = 4
 def musicOff():
     #GPIO.output(LED_PIN, GPIO.input(PIR_PIN))
     print("music playing")
-    time.sleep(2)
+    time.sleep(.5)
     print("music off")
+    pygame.mixer.music.stop()
     #GPIO.output(LED_PIN, False)
 try:
 	while True:
@@ -28,6 +39,7 @@ try:
 			print("Motion Detected!")
 			t = Thread(target=musicOff) # Create thread
 			t.start() # Start thread
+			pygame.mixer.music.play()
 		time.sleep(4)
 except KeyboardInterrupt:
 	print("interupt")
